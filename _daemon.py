a40635b583b6e7d31efff3ba6835a9f33a5f1440
Eugene <ujin@i.ua>
2017-10-06 16:44:43 +0300

    daemon lib

--- _daemon.py
+++ _daemon.py
@@ -7,6 +7,9 @@
 import daemon, signal
 from daemon import pidfile
 import threading
+import logging
+import logging.handlers
+import time
 
 CONS_FORMAT = "%(asctime)s:%(levelname)s:%(name)s:%(message)s"
 SYSLOG_FORMAT = "%(levelname)s:%(name)s:%(message)s"
@@ -15,7 +18,7 @@ class CDaemon(object):
     """docstring for CDaemon"""
     def __init__(self, arg=None):
         super(CDaemon, self).__init__()
-        self._conf = arg
+        self.__conf = arg
         self._thread = None
         self._start()
 
@@ -29,20 +32,20 @@ class CDaemon(object):
         self.log = logging.getLogger(self._conf('name'))
 
     def _conf(self, arg=None):
-        if self._conf:
-            if self._conf[arg]:
-                return self._conf[arg]
+        if self.__conf:
+            if self.__conf.get(arg):
+                return self.__conf[arg] 
         return None
 
     def _start(self):
-        self._running = True
+        self.exit_flag = threading.Event()
         if self._thread is None:
             self._thread = threading.Thread(target=self._run)
             self._thread.daemon = True
             self._thread.start()
 
     def _stop(self):
-        self._running = False
+        self.exit_flag.set()
         if self._thread is not None:
             if threading.current_thread() != self._thread:
                 self._thread.join()
@@ -52,13 +55,13 @@ class CDaemon(object):
         self._setLog()
         self.log.info("Start thread")
         self.on_start()
-        while self._running:
-            time.sleep(1)
+        while not self.exit_flag.wait(timeout=1):
             self.on_run()
         self.on_stop()
         self.log.info("Exit thread")
 
     def close(self):
+        self.on_close()
         self._stop()
 
     def on_start(self):
@@ -71,4 +74,7 @@ class CDaemon(object):
         pass
 
     def on_init(self):
+        pass
+
+    def on_close(self):
         pass
--- esdmd.py
+++ esdmd.py
@@ -15,73 +15,46 @@ import os, sys, time
 import threading
 # from kafka import KafkaConsumer
 # from kafka.errors import KafkaError
+from _daemon import CDaemon
 
 PROG=os.path.basename(sys.argv[0]).rstrip('.py')
 PROG_DESC='ESDM daemon'
 
-class cESDM(object):
-    """docstring for cESDM"""
-    def __init__(self, debugging=False):
-        super(cESDM, self).__init__()
-        self.debugging = debugging
-        self.log = logging.getLogger(PROG)
-        if debugging == True:
-            self.log.setLevel(logging.DEBUG)
-        else:
-            self.log.setLevel(logging.INFO)
-        lh = logging.handlers.SysLogHandler()
-        lh.setFormatter(logging.Formatter('%(filename)s[%(process)d]: %(message)s'))
-        self.log.addHandler(lh)
-        self._thread = None
-        self._start()
+CONS_FORMAT = "%(asctime)s:%(levelname)s:%(name)s:%(message)s"
+SYSLOG_FORMAT = "%(levelname)s:%(name)s:%(message)s"        
 
-    def _start(self):
-        self._running = True
-        if self._thread is None:
-            self._thread = threading.Thread(target=self._run)
-            self._thread.daemon = True
-            self._thread.start()
+DEFAULT_CONFIG={
+    'name':PROG,
+}
 
-    def _stop(self):
-        self._running = False
-        if self._thread is not None:
-            if threading.current_thread() != self._thread:
-                self._thread.join()
-                self._thread = None
+def namedtuple_asdict(t):
+    return({str(type(t).__name__) : dict(t._asdict())})
 
-    def _run(self):
-        self.log.info("Start thread")
-        while self._running:
-            time.sleep(.3)
-        self.log.info("Exit thread")
+class ESDM(CDaemon):
+    """docstring for ESDM"""
+    def on_run(self):
+        print 1        
 
-    def close(self):
-        self._stop()
-
-def run_program(is_debug=False, foreground=False):
-    smmon = cESDM(is_debug)
+def run_program(foreground=False):
     if foreground:
         print "Start", PROG
-    while True:
-        try:
+    # esdm = cESDM(DEFAULT_CONFIG)
+    esdm = ESDM(DEFAULT_CONFIG)
+    try:
+        while True:
             time.sleep(.5)
-        except KeyboardInterrupt:
-            smmon.close()
-            if foreground:
-                print "\nExit", PROG
-                sys.exit()
-            else:
-                time.sleep(.5)
+    except KeyboardInterrupt:
+        esdm.close()
+        if foreground:
+            print "Exit", PROG
+            sys.exit()
+    except:
+        esdm.close()
+        time.sleep(.5)
 
-def start_daemon(pidf, logf, is_debug=False):
+def start_daemon(pidf, logf):
     ### This launches the daemon in its context
-    logger = logging.getLogger(PROG+'err')
-    logger.setLevel(logging.DEBUG)
     fh = logging.FileHandler(logf)
-    formatter = logging.Formatter('%(asctime)s %(levelname)s: %(message)s')
-    fh.setFormatter(formatter)
-    logger.addHandler(fh)
-    logger.info("start daemon")
     with daemon.DaemonContext(
         working_directory='/tmp',
         umask=0o002,
@@ -89,12 +62,23 @@ def start_daemon(pidf, logf, is_debug=False):
         stderr =  fh.stream,
         stdout =  fh.stream,
         ) as context:
-            run_program(is_debug)
+            run_program()
 
 def stop_daemon(pidf):
     if os.path.isfile(pidf):
         pid = int(open(pidf).read())
-        os.kill(pid, signal.SIGTERM)
+        try:
+            os.kill(pid, signal.SIGTERM)
+        except OSError as e:
+            os.remove(pidf)
+        except Exception as e:
+            raise
+        else:
+            pass
+        finally:
+            pass
+    else:
+        print "dd"
         
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description=PROG_DESC)
@@ -108,8 +92,19 @@ if __name__ == "__main__":
     parser.add_argument('-c', '--config', default='/etc/'+ PROG +'.conf')
     args = parser.parse_args()
 
+    if args.debug:
+        loglevel = logging.DEBUG
+    else:
+        loglevel = logging.INFO
+
+    if args.foreground:
+        logging.basicConfig(level=loglevel, format=CONS_FORMAT)
+
+    logger = logging.getLogger()
+    logger.setLevel(loglevel)
+
     if args.start:
-        start_daemon(pidf=args.pid_file, logf=args.log_err, is_debug=args.debug )
+        start_daemon(pidf=args.pid_file, logf=args.log_err )
     elif args.stop:
         stop_daemon(pidf=args.pid_file)
     elif args.restart:
@@ -117,6 +112,6 @@ if __name__ == "__main__":
         time.sleep(1)
         start_daemon(pidf=args.pid_file, logf=args.log_err)
     elif args.foreground:
-        run_program(is_debug=args.debug, foreground=True)
+        run_program(foreground=True)
     else:
         parser.print_help()
