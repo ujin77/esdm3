67dabc300cefa4e7f0fe305dbbcc4d299ea57475
Eugene Mogilevsky <ujin77@i.ua>
2018-05-04 12:20:53 +0300

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='esdmd',
+    version='1.0',
+    packages=['python-daemon','lockfile','minimalmodbus','py-zabbix','paho-mqtt'],
+    url='https://github.com/ujin77/esdmd',
+    license='',
+    author='Eugene M.',
+    author_email='ujin@i.ua',
+    description='Eastron electricity meter daemon'
+)
